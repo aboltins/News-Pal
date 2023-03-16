296f8a30de98c2bfd70145a487c7b090451dc98b
CoralPeony <coralpeony21@gmail.com>
2023-03-16 14:26:16 +0000

    updated the clock function

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import UserPage from "./pages/UserPage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Clock from "react-clock";
+
 
 function App() {
   return (
     <UserAuthContextProvider>
       <Router>
-        <Clock />
+        
         <Header />
 
         <Routes>
