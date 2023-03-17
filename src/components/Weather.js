517104d058ba6afb7c4036222a30f3948a8bb73a
CoralPeony <coralpeony21@gmail.com>
2023-03-17 14:49:19 +0000

    added weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,37 @@
-import React from "react";
 
-const Weather = () => {
+import React, { useEffect, useState } from "react";
+import WeatherCard from './WeatherCard';
+
+function Weather() {
+  
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(async function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+  
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`);
+        const result = await response.json();
+        setData(result);
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, []);
+  
   return (
-    <div>
+    <div className="App">
+      {(typeof data.main != 'undefined') ? (
+        <WeatherCard weatherData={data}/>
+      ): (
+        <div></div>
+      )}
+      
     </div>
   );
-};
-
+}
 export default Weather;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'semantic-ui-css/semantic.min.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
