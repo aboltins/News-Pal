1277dffa69c352813e315eec4de69304308a2b5b
CoralPeony <coralpeony21@gmail.com>
2023-03-18 17:33:19 +0000

    added additional info to weather card

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,35 +1,59 @@
-import React from 'react';
-import './styles.css';
-import moment from 'moment';
-import { Button } from 'semantic-ui-react';
+import React from "react";
+import styles from "../styles/Weather.module.css";
+import moment from "moment";
+import { Button } from "semantic-ui-react";
 
 const refresh = () => {
   window.location.reload();
-}
+};
 
-const WeatherCard = ({weatherData}) => (
-  <div className="main">
+const WeatherCard = ({ weatherData }) => (
+  <div className={styles.main}>
+    <div className={styles.top}>
+      <p className={styles.header}>{weatherData.name}</p>
+      <Button
+        className={styles.button}
+        inverted
+        color="olive"
+        circular
+        icon="refresh"
+        onClick={refresh}
+      />
+    </div>
+    <div className={styles.flex}>
+      <p className={styles.day}>
+        {moment().format("dddd")}, <span>{moment().format("LL")}</span>
+      </p>
+      <img
+       
+        src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`}
+        alt={weatherData.weather[0].main}
+      />
+    </div>
 
-      <div className="top">
-        <p className="header">{weatherData.name}</p>
-        <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
-      </div>
-      <div className="flex">
-        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-        <p className="description">{weatherData.weather[0].main}</p>
-      </div>
+    <div className={styles.flex}>
+      <p className={styles.temp}>Temperature: {weatherData.main.temp} &deg;C</p>
+      <p className={styles.temp}>{weatherData.weather[0].description.toUpperCase()}</p>
+    </div>
 
-      <div className="flex">
-        <p className="temp">Temperature: {weatherData.main.temp} &deg;C</p>
-        <p className="temp">Humidity: {weatherData.main.humidity} %</p>
-      </div>
+    <div className={styles.flex}>
+    <p className={styles.temp}>Feels like: {weatherData.main.feels_like} &deg;C</p>
+      
+      <p className={styles.temp}>Humidity: {weatherData.main.humidity} %</p>
+    </div>
 
-      <div className="flex">
-        <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-UK')}</p>
-        <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-UK')}</p>
-      </div>
-    
+    <div className={styles.flex}>
+      <p className={styles.temp}>
+        Sunrise:{" "}
+        {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString("en-UK")}
+      </p>
+      <p className={styles.temp}>
+        Sunset:{" "}
+        {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString("en-UK")}
+      </p>
+    </div>
+
   </div>
-)
+);
 
 export default WeatherCard;
