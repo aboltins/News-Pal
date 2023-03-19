68672a15fca61f75afac508ca2da8a317d2095f9
CoralPeony <coralpeony21@gmail.com>
2023-03-19 15:59:43 +0000

    added search function to generate data results from API

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
-import styles from "../styles/SearchBar.css";
+import { useState, useEffect } from "react";
+import "../styles/SearchBar.css"
+const API_KEY = 'be2ad656-3350-4f96-b3d1-d5329e39a5ed'
 
 const SearchBar = () => {
   const [searchInput, setSearchInput] = useState("");
   const [isActive, setIsActive] = useState(false);
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
 
   function toggleSearch() {
     setIsActive(!isActive);
@@ -12,19 +15,27 @@ const SearchBar = () => {
   const handleChange = (e) => {
     e.preventDefault();
     setSearchInput(e.target.value);
+    setQuery(e.target.value);
   };
 
   const handleClear = () => {
-    document.getElementById("mysearch").value = "";
+    setSearchInput("");
   };
+
+  const fetchApiData = async () => {
+    const response = await fetch(`https://content.guardianapis.com/search?q=${query}&api-key=${API_KEY}`);
+    const data = await response.json();
+    setData(data.response.results);
+  };
+  
+  useEffect(() => {
+    fetchApiData();
+  }, [query, fetchApiData]);
 
   return (
     <div className="container">
       <div className={`search ${isActive ? "active" : ""}`}>
-        <div
-          className={`icon ${isActive ? "active" : ""}`}
-          onClick={toggleSearch}
-        ></div>
+        <div className={`icon ${isActive ? "active" : ""}`} onClick={toggleSearch}></div>
         <input
           id="mysearch"
           className="input"
@@ -35,6 +46,16 @@ const SearchBar = () => {
         />
         <span className="clear" onClick={handleClear}></span>
       </div>
+      {query !== "" && (
+        <div>
+          {data.map((result) => (
+            <div key={result.id}>
+              <h2>{result.webTitle}</h2>
+              <p>{result.webUrl}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
